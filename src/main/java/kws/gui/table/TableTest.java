479773989f96f522f2ec93f17e91cc3d85fec1ee
Sampath Chandana <kwsampath@gmail.com>
2012-08-01 19:21:20 +0530

    Added JTable Example

---
+++ src/main/java/kws/gui/table/TableTest.java
@@ -0,0 +1,16 @@
+package kws.gui.table;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chan
+ * Date: 8/1/12
+ * Time: 7:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TableTest extends JFrame {
+    public static void main(String[] args) {
+
+    }
+}
